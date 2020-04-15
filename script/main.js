22eba0613f9fefe97462e16b8eedd9ad1fecdee5
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-15 16:57:35 +0200

    JS
    Logica game finita.

--- script/main.js
+++ script/main.js
@@ -3,9 +3,9 @@
  * Esercizio :  Simon Says
  * 
  * Descrizione:
- *  1) Un alert espone 5 numeri casuali (univoci). 
- *  2) Da li parte un timer di 30 secondi.
- *  3) Dopo 30 secondi l’ utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+ *  1) Un alert espone 5 numeri casuali (univoci). <
+ *  2) Da li parte un timer di 30 secondi. <
+ *  3) Dopo 30 secondi l’ utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente. <
  *  4) Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
  *  5) Assicuratevi di saper fare correttamente l’ esercizio con output base (alert() , prompt() , console.log() )
  * 
@@ -13,10 +13,49 @@
 
 jQuery(document).ready(function ($) {
     
-    //console.log('si');
-    
-    console.log( checkNum() );
+    //console.log('si');  
+    //var listNumberUser = userNumberInsert();
+    // console.log(listNumberUser);
 
+    // Setup
+    var listNumberRandom = checkNum();
+    alert( 'I numeri da memorizzare sono : ' + listNumberRandom );
+
+    setTimeout( function() {
+
+        var listNumberUser = userNumberInsert(); 
+        
+        
+        var elementiUguali = [];
+        var elementiDiversi = [];
+
+        for ( var i = 0; i < listNumberRandom.length; i++) {
+            
+            
+
+            if ( listNumberRandom.includes( listNumberUser[i] ) ) {
+
+                elementiUguali.push( listNumberUser[i] );
+
+            } else {
+
+                elementiDiversi.push( listNumberUser[i] );
+
+            }
+            
+            
+        }
+
+        console.log( 'Lista numeri random : ' + listNumberRandom);
+        console.log( 'Numeri inseriti dall utente : ' + listNumberUser);
+        console.log( 'Gli elementi uguali sono : ' + elementiUguali);
+        console.log( 'Gli elementi diversi sono : ' + elementiDiversi);
+            
+        
+    }, 30000);
+
+    
+  
 
 });  // <------ End Page
 
@@ -26,7 +65,7 @@ function randomNum( min, max ) {
 
 }
 
-function checkNum() {
+function checkNum() { // Restituisce una lista di numeri controllati 
 
     var listNum = [];
 
@@ -34,7 +73,7 @@ function checkNum() {
         
         var randomNumber = randomNum( 1, 5 );
         
-        if ( listNum.includes(randomNumber) !== true ) {
+        if ( listNum.includes( randomNumber ) !== true ) {
 
             listNum.push( randomNumber );
         }
@@ -42,4 +81,18 @@ function checkNum() {
     }
 
     return listNum;
+}
+
+function userNumberInsert () { //Richiede 5 numeri all utente
+
+    var listNumberUser = [];
+
+    for ( var i = 0; i < 5; i++ ) {
+        
+        var userNum = parseInt( prompt( 'Inserisci un numero : ' ) );
+        listNumberUser.push( userNum );
+    }
+
+    return listNumberUser;
+
 }
