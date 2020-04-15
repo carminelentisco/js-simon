65d04cf1d7aefcadd83ee28f3b74e422224ce967
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-15 14:27:49 +0200

    JS
    Struttura base.

---
+++ script/main.js
@@ -0,0 +1,21 @@
+/***************************************************************************************************************************\
+ * 
+ * Esercizio :  Simon Says
+ * 
+ * Descrizione:
+ *  1) Un alert espone 5 numeri casuali (univoci). 
+ *  2) Da li parte un timer di 30 secondi.
+ *  3) Dopo 30 secondi l’ utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+ *  4) Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
+ *  5) Assicuratevi di saper fare correttamente l’ esercizio con output base (alert() , prompt() , console.log() )
+ * 
+\***************************************************************************************************************************/
+
+jQuery(document).ready(function ($) {
+    
+    //console.log('si');
+
+    
+
+
+});  // <------ End Page
